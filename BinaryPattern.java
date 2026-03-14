725d24dee883aa5ed37ca048cf02384dd6979804
Priyadarshani Raut <rautpriya286@gmail.com>
2026-03-14 13:48:59 +0530

    Add BinaryPattern class to print binary pattern

---
+++ BinaryPattern.java
@@ -0,0 +1,16 @@
+public class BinaryPattern {
+    public static void main(String[] args) {
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+            int val = (i % 2 == 1) ? 1 : 0;
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(val + " ");
+                val = (val == 1) ? 0 : 1;
+            }
+
+            System.out.println();
+        }
+    }
+}
